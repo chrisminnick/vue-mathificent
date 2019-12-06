e48bf648f6ec4c95eb609f5042272ff5d8626e92
Chris Minnick <chris@minnick.com>
2019-12-06 12:01:02 -0800

    working on vue app

--- mathificent/src/App.vue
+++ mathificent/src/App.vue
@@ -1,6 +1,28 @@
 <template>
   <div id="app">
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="/">Mathificent</a>
+        <button className="navbar-toggler" type="button"
+        data-toggle="collapse" data-target="#navbarText">
+        <span className="navbar-toggler-icon"></span>
+        </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+              <a className="nav-link" href="/">Home</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+
     <h1>Mathificent</h1>
+      <footer className="navbar fixed-bottom bg-dark">
+        <a href="https://www.webucator.com">
+        Copyright &copy; {new Date().getFullYear()} Webucator
+        </a>
+      </footer>
   </div>
 </template>
 
