57ac68b473e0f804378f8e1cc3f8830bd9c28c16
Chris Minnick <chrisminnick@Chriss-Mac-mini.local>
2019-12-25 04:52:28 -0800

    fixed routing

--- mathificent/src/components/Main.vue
+++ mathificent/src/components/Main.vue
@@ -1,30 +1,33 @@
 <template>
-    <div>
+<div>
+    <div v-if="screen==='config'">
         <SelectInput :currentValue="currentOperation" label="Operation" id="operation" v-model="currentOperation" :options="operations" />
         <SelectInput :currentValue="maxNumber" label="Maximum Number" id="max-number" v-model="maxNumber" :options="numbers" />
         <ScoreOutputBox />
-        <PlayButton />
+        <button v-on:click="play()">Play!</button>
         <p>current operation: {{currentOperation}}</p>
         <p>max number: {{maxNumber}}</p>
     </div>
+    <div v-if="screen==='play'">
+        <button v-on:click="config()">Change the Game!</button>
+    </div>
+</div>
 </template>
 <script>
 import SelectInput from './SelectInput';
 import ScoreOutputBox from './ScoreOutputBox';
-import PlayButton from './PlayButton';
 
 export default {
     name: 'Main',
     components: {
         SelectInput,
-        ScoreOutputBox,
-        PlayButton
-    },
+        ScoreOutputBox    },
     data: function(){
         return {
         operations: ['+','x','/','-'],
-        currentOperation:'+',
-        maxNumber:2
+        currentOperation: '+',
+        maxNumber: 2,
+        screen: 'config'
         }
     },
     computed: {
@@ -34,6 +37,14 @@ export default {
                 numbers.push(number);
             }
             return numbers;
+        }
+    },
+    methods: {
+        config() {
+            this.screen = "config";
+        },
+        play() {
+            this.screen = "play";
         }
     }
     
